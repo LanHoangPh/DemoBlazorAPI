236808d5895823c47a0a7574d45e2fb60d763e4a
dothienkhoi <dothienkhoi22022005@gmail.com>
2025-06-03 22:33:50 +0700

    hi

--- APIWeb/Data/AppDbContext.cs
+++ APIWeb/Data/AppDbContext.cs
@@ -4,9 +4,7 @@ namespace APIWeb.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+        
        
     }
 }
